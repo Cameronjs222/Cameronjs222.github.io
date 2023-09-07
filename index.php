d8509ff7a6fb676c377e33bdc437f380844d0ee6
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-09-07 14:09:14 -0700

    index.php

--- index.php
+++ index.php
@@ -12,7 +12,27 @@
         <li><a href="index.php?page=about">About</a></li>
         <li><a href="index.php?page=contact">Contact</a></li>
     </ul>
-    
+
     </nav>
+
+    <main>
+        <?php
+        if(isset($_GET['page'])){
+            $page = $_GET['page'];
+            include($page . ".php");
+        }else{
+            include("home.php");
+        }
+        ?>
+    </main>
+    
+
+
+
+    <footer>
+        <p>Footer</p>
+    </footer>
+
+
 </body>
 </html>
