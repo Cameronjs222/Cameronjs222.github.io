b47a1eeadc96e0de20d4216727f1db2f58dfc8c0
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-10-02 16:54:11 -0700

    fixed pacing of scroll

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const scrollImg = () => {
     const windowHeight = window.innerHeight;
     const endOfPage = windowHeight * 5/3;
     const scrollRatio = endOfPage / windowHeight;
-    img.style.top = `-${imgHeight - scrollPos * 1.35}px`;
+    img.style.top = `-${imgHeight - scrollPos}px`;
     const secondImg = document.querySelector('.img2');
     const secondImgHeight = secondImg.clientHeight;
     console.log(endOfPage)
     if (scrollPos >= endOfPage / 2) {
-        secondImg.style.top = `-${secondImgHeight + windowHeight - scrollPos * 1.5}px`;
+        secondImg.style.top = `-${secondImgHeight + windowHeight - scrollPos}px`;
     }
     console.log(scrollPos, windowHeight);
     console.log(scrollRatio + '%');
