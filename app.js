33e85073a9dcb89705bedfae1ae0d87ac3a230d9
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-10-10 23:25:55 -0700

    Remade scroll function for scallabilyty

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
+
+const img = document.querySelector('.img');
+console.log(img.clientHeight);
+const secondImg = document.querySelector('.img2');
+console.log(secondImg.clientHeight);
+const projectWindow = document.querySelector('.projects')
+console.log(projectWindow.clientHeight + " window height");
+img.style.top = `-${projectWindow.clientHeight}px`;
+secondImg.style.top = `-${projectWindow.clientHeight}px`;
+
 const scrollImg = () => {
     const scrollPos = window.scrollY;
-    const img = document.querySelector('.img');
-    const imgHeight = img.clientHeight;
-    const windowHeight = window.innerHeight;
-    const endOfPage = windowHeight * 5/3;
-    const scrollRatio = endOfPage / windowHeight;
-    img.style.top = `-${imgHeight - scrollPos}px`;
-    const secondImg = document.querySelector('.img2');
-    const secondImgHeight = secondImg.clientHeight;
-    console.log(endOfPage)
-    if (scrollPos >= endOfPage / 2) {
-        secondImg.style.top = `-${secondImgHeight + windowHeight - scrollPos}px`;
+    if (scrollPos >= window2.getBoundingClientRect().top) {
+        secondImg.style.top = `-${projectWindow.clientHeight + secondImg.clientHeight - scrollPos}px`;
     }
+    img.style.top = `-${projectWindow.clientHeight - scrollPos}px`;
 }
 
+
+const window1 = document.getElementById('firstWindow');
+const window2 = document.getElementById('secondWindow');
+console.log(window1.getBoundingClientRect().y + " top");
+console.log(window2.getBoundingClientRect().y + " bottom");
+
+
+
+// const scrollImg = () => {
+//     const scrollPos = window.scrollY;
+//     const img = document.querySelector('.img');
+//     const imgHeight = img.clientHeight;
+//     const projectWindow = window.innerHeight;
+//     const endOfPage = projectWindow * 5/3;
+//     const scrollRatio = endOfPage / projectWindow;
+//     img.style.top = `-${imgHeight - scrollPos}px`;
+//     const secondImg = document.querySelector('.img2');
+//     const secondImgHeight = secondImg.clientHeight;
+//     console.log(endOfPage)
+//     if (scrollPos >= endOfPage / 2) {
+//         secondImg.style.top = `-${secondImgHeight + projectWindow - scrollPos}px`;
+//     }
+// }
 
 const textOpacity = () => {
     const scrollPos = window.scrollY;
