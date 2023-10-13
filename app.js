b08df78a2842700e87b81932d0b81e6f4ee5cfe2
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-10-13 01:23:36 -0700

    No longer need text opacity

--- app.js
+++ app.js
@@ -17,18 +17,18 @@ const scrollImg = () => {
 const window1 = document.getElementById('firstWindow');
 const window2 = document.getElementById('secondWindow');
 
-const textOpacity = () => {
-    const scrollPos = window.scrollY;
-    const text1 = document.querySelector('.text1');
-    const text2 = document.querySelector('.text2');
-    if (scrollPos > window1.getBoundingClientRect().top - window.innerHeight) {
-      text1.style.color = `rgb(255, 255, 255, ${scrollPos / (window.innerHeight / 2)})`;
-    }
-    console.log(scrollPos - window2.getBoundingClientRect().top);
-    if (scrollPos > window2.getBoundingClientRect().top) {
-      text2.style.color = `rgb(255, 255, 255, ${(scrollPos - window2.getBoundingClientRect().top) / (window.innerHeight / 2)}`;
-    }
-  }
+// const textOpacity = () => {
+//     const scrollPos = window.scrollY;
+//     const text1 = document.querySelector('.text1');
+//     const text2 = document.querySelector('.text2');
+//     if (scrollPos > window1.getBoundingClientRect().top - window.innerHeight) {
+//       text1.style.color = `rgb(255, 255, 255, ${scrollPos / (window.innerHeight / 2)})`;
+//     }
+//     console.log(scrollPos - window2.getBoundingClientRect().top);
+//     if (scrollPos > window2.getBoundingClientRect().top) {
+//       text2.style.color = `rgb(255, 255, 255, ${(scrollPos - window2.getBoundingClientRect().top) / (window.innerHeight / 2)}`;
+//     }
+//   }
 
 // const textOpacity = () => {
 //     const scrollPos = window.scrollY;
@@ -77,5 +77,5 @@ const emailLink = () => {
 }
 
 emailButton.addEventListener("click", emailLink);
-window.addEventListener('scroll', textOpacity);
+// window.addEventListener('scroll', textOpacity);
 window.addEventListener('scroll', scrollImg);
