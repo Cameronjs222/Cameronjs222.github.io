753a10a2e7b2a17cd53a310be81b8571316641c3
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2024-01-22 19:35:49 -0800

    created new index with highlight mouse

--- app.js
+++ app.js
@@ -15,55 +15,11 @@ const scrollImg = () => {
 
 const window1 = document.getElementById('firstWindow');
 const window2 = document.getElementById('secondWindow');
-// const textOpacity = () => {
-//     const scrollPos = window.scrollY;
-//     const text1 = document.querySelector('.text1');
-//     const text2 = document.querySelector('.text2');
-//     if (scrollPos > window1.getBoundingClientRect().top - window.innerHeight) {
-//       text1.style.color = `rgb(255, 255, 255, ${scrollPos / (window.innerHeight / 2)})`;
-//     }
-//     console.log(scrollPos - window2.getBoundingClientRect().top);
-//     if (scrollPos > window2.getBoundingClientRect().top) {
-//       text2.style.color = `rgb(255, 255, 255, ${(scrollPos - window2.getBoundingClientRect().top) / (window.innerHeight / 2)}`;
-//     }
-//   }
 
-// const textOpacity = () => {
-//     const scrollPos = window.scrollY;
-//     const text1 = document.querySelector('.text1');
-//     const text2 = document.querySelector('.text2');
-//     if (scrollPos > window1.getBoundingClientRect().top - window.innerHeight) {
-//         text1.style.opacity = `${scrollPos / (window.innerHeight/2)}`;
-//     } 
-//     console.log(scrollPos - window2.getBoundingClientRect().top);
-//     if (scrollPos > window2.getBoundingClientRect().top) {
-//         text2.style.opacity = `${(scrollPos - window2.getBoundingClientRect().top) / (window.innerHeight)}`;
-//         text2.style.color = "white";
-//     }
-// }
-
-// const textOpacity = () => {
-//     const scrollPos = window.scrollY;
-    
-//     // Select all elements with class name "text1"
-//     const text1Elements = document.querySelectorAll('.text1');
-    
-//     // Select all elements with class name "text2"
-//     const text2Elements = document.querySelectorAll('.text2');
-    
-//     text1Elements.forEach(text1 => {
-//         if (scrollPos > text1.getBoundingClientRect().top - window.innerHeight) {
-//             text1.style.opacity = `${scrollPos / (window.innerHeight / 2)}`;
-//         }
-//     });
-    
-//     text2Elements.forEach(text2 => {
-//         if (scrollPos > text2.getBoundingClientRect().top) {
-//             text2.style.opacity = `${(scrollPos - text2.getBoundingClientRect().top) / window.innerHeight}`;
-//             text2.style.color = "white";
-//         }
-//     });
-// }
+document.getElementById('navigateButton').addEventListener('click', function() {
+    // Redirect to the next page (nextPage.html)
+    window.location.href = 'newIndex.html';
+});
 
 
 const emailButton = document.getElementById("emailButton");
